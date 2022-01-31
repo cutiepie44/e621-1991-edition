342fafc17c5b1614237f15ba993fd1d8edcb023e
Lizzie <74326237+cutiepie44@users.noreply.github.com>
2022-01-31 11:17:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ print('''
 WELCOME TO ALPHA 0.0.1 of e621 1991
 ###################################
 ''')
-code = input("INSERT A CODE:")
-headers = {"User-Agent":"LizzieTheWitch"}
-site0 = "https://e621.net/posts/"
-site = site0+code
-response = requests.get(site, headers=headers)
-api_key = "da3DG9QGd6qsyUp4htgqcs4B"
-soup = BeautifulSoup(response.text, 'html.parser')
-img_tags = soup.find_all('img')
+star = 0
+if star == "0":
+    star = 0
+while True:
+    code = input("INSERT A CODE:")
+    headers = {"User-Agent":"LizzieTheWitch"}
+    site0 = "https://e621.net/posts/"
+    site = site0+code
+    response = requests.get(site, headers=headers)
+    api_key = "da3DG9QGd6qsyUp4htgqcs4B"
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_tags = soup.find_all('img')
+    urls = [img['src'] for img in img_tags]
 
-urls = [img['src'] for img in img_tags]
-
-for url in urls:
-    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-    if not filename:
-         print("Regex didn't match with the url: {}".format(url))
-         continue
-    with open(filename.group(1), 'wb') as f:
-        if 'http' not in url:
-            url = '{}{}'.format(site, url)
-        response = requests.get(url)
-        f.write(response.content)
+    for url in urls:
+        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+        if not filename:
+            print("Regex didn't match with the url: {}".format(url))
+            continue
+        with open(filename.group(1), 'wb') as f:
+            if 'http' not in url:
+                url = '{}{}'.format(site, url)
+            response = requests.get(url)
+            f.write(response.content)
